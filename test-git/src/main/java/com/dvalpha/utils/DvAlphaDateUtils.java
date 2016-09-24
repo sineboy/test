bdfa0c4358bee111b87c8d3741762b1bfb3e3cc7
alex <alex@192.168.1.35>
2016-09-24 07:01:25 +0100

    Resubido el nuevo codigo con los comentarios

--- test-git/src/main/java/com/dvalpha/utils/DvAlphaDateUtils.java
+++ test-git/src/main/java/com/dvalpha/utils/DvAlphaDateUtils.java
@@ -94,9 +94,14 @@ public class DvAlphaDateUtils {
  
       DvAlphaDateUtils util = new DvAlphaDateUtils();
       Date salida = new Date();
-        util.fechahoyCompareFechaSalida(salida);
-                
-           
+      util.fechahoyCompareFechaSalida(salida);
+       
+      //Test
+      int res= util.fechahoyCompareFechaSalida(salida);
+      Date semanaMenos=util.sumarRestarDiasFecha(salida, -10);
+        
+      System.out.println("compara :"+res);
+      System.out.println("Resta :"+semanaMenos);    
  
  
  
